604ce7116c5fc6871dae1d93faf73c9b00c47477
Ayaan Mirza Baig <ayaanmirzabaig85@gmail.com>
2025-03-25 17:45:17 +0530

    Add Kernel Builder

---
+++ kali-nethunter-kernel-builder/out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 5 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#5 SMP PREEMPT Tue Mar 25 17:03:41 IST 2025"
+#define LINUX_COMPILE_BY "amb"
+#define LINUX_COMPILE_HOST "ideapad"
+#define LINUX_COMPILER "Android (6207600 based on r377782b) clang version 10.0.4 (https://android.googlesource.com/toolchain/llvm-project 0cb1afdb060d6619a9837ab6866cf4adf2336416), GNU ld (GNU Binutils) 2.27.0.20170315"
